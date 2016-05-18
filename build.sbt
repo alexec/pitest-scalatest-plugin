3a151a7b3612d68a3d265095659cff63df046be8
Alex Collins <alex.e.c@gmail.com>
2016-05-18 11:16:55 +0100

    Copied code from pit-subt.

---
+++ build.sbt
@@ -0,0 +1,12 @@
+
+name := "pitest-scalatest-plugin"
+
+organization := "org.pitest"
+
+version := "1.1.10-SNAPSHOT"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.pitest" % "pitest" % "1.1.10"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6"
---
+++ src/main/scala/org/pitest/scalatest/ScalaTestPluginFactory.scala
@@ -0,0 +1,10 @@
+package org.pitest.scalatest
+
+import org.pitest.classinfo.ClassByteArraySource
+import org.pitest.testapi.{Configuration, TestGroupConfig, TestPluginFactory}
+
+class ScalaTestPluginFactory extends TestPluginFactory {
+  override def createTestFrameworkConfiguration(config: TestGroupConfig, source: ClassByteArraySource): Configuration = new ScalaTestConfiguration
+
+  override def description(): String = "ScalaTest"
+}
