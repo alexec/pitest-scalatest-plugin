71b800d84325ff7b843fabf9adf0975e5aeb8ac1
Alex Collins <alex.e.c@gmail.com>
2016-05-18 15:38:48 +0100

    correctly run test

--- src/main/scala/org/pitest/scalatest/ScalaTestUnit.scala
+++ src/main/scala/org/pitest/scalatest/ScalaTestUnit.scala
@@ -1,8 +1,9 @@
 package org.pitest.scalatest
 
 import org.pitest.testapi.{AbstractTestUnit, Description, ResultCollector}
-import org.pitest.util.{IsolationUtils, Unchecked}
-import org.scalatest.Suite
+import org.pitest.util.IsolationUtils
+import org.scalatest._
+import org.scalatest.events._
 
 class ScalaTestUnit(clazz: Class[_]) extends AbstractTestUnit(new Description(clazz.getName, clazz)) {
   override def execute(loader: ClassLoader, rc: ResultCollector) = {
@@ -12,11 +13,27 @@ class ScalaTestUnit(clazz: Class[_]) extends AbstractTestUnit(new Description(cl
 
     rc.notifyStart(getDescription)
     try {
-      suite.execute()
-    } catch {
-      case t: Throwable => throw Unchecked.translateCheckedException(t)
-    } finally {
+      suite.run(
+        None,
+        new Reporter {
+          override def apply(event: Event): Unit = {
+            event match {
+              case e: TestFailed => throw new AssertionError(event.toString)
+              case e: TestCanceled => throw new AssertionError(event.toString)
+              case e: SuiteAborted => throw new AssertionError(event.toString)
+              case _ =>
+            }
+          }
+        },
+        Stopper.default,
+        Filter(),
+        Map(),
+        Option.empty,
+        new Tracker()
+      )
       rc.notifyEnd(getDescription)
+    } catch {
+      case t: Throwable => rc.notifyEnd(getDescription, t)
     }
   }
 }
